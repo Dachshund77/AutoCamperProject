5960f08cb925435d4cfea8f6bec301ed8880794d
Sven Büchner <sven.buechner.privat@googlemail.com>
2019-04-23 11:54:08 +0200

    Half finsihed stuff.... sorry

--- src/Foundation/DB.java
+++ src/Foundation/DB.java
@@ -3,6 +3,7 @@ package Foundation;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Properties;
@@ -17,10 +18,12 @@ public class DB {
     private static Connection conn;
     private static CallableStatement cstmt;
     private static ResultSet rs;
+    private static ResultSetMetaData rsmd;
 
-    private DB(){
+    private DB() {
 
     }
+
     static {
         Properties props = new Properties();
         java.lang.String fileName = "src/Foundation/db.properties";
@@ -39,33 +42,35 @@ public class DB {
     }
 
     //TODO method to execute a single stored procedure
-    public static ArrayList<Object[]> executeStoredProcedure(String sp, Object... param){
+    public static ArrayList<Object[]> executeStoredProcedure(String sp, Object... param) {
+        rsmd = null;
         ArrayList<Object[]> returnArrayList = new ArrayList<>();
         try {
             // Preparing metaData
-            ArrayList<ProcedureMetaData> metaData = getSPMetaData(sp);
+            ArrayList<ProcedureMetaData> metaDataArray = getSPMetaData(sp);
             //Setting callable statement
-            cstmt = conn.prepareCall(buildProcedureCall(sp, metaData.size()));
+            cstmt = conn.prepareCall(buildProcedureCall(sp, metaDataArray.size()));
             //Build parameters
-            for (int i = 0; i < metaData.size(); i++) {
-                ProcedureMetaData tempMetaData = metaData.get(i);
-                setCallParameter(tempMetaData.getPosition(),tempMetaData.getDataType(), param[i]);
+            for (int i = 0; i < metaDataArray.size(); i++) {
+                ProcedureMetaData tempMetaData = metaDataArray.get(i);
+                setCallParameter(tempMetaData.getPosition(), tempMetaData.getDataType(), param[i]);
             }
             //Getting results
             rs = cstmt.executeQuery();
-            Integer noOfColumns = rs.getMetaData().getColumnCount();
+            rsmd = rs.getMetaData();
+            Integer noOfColumns = rsmd.getColumnCount();
             // return values
-            while (rs.next()){
+            while (rs.next()) {
                 Object[] tempArray = new Object[noOfColumns];
                 for (int i = 0; i < tempArray.length; i++) {
-                    tempArray[i] = rs.getObject(i+1);
+                    tempArray[i] = rs.getObject(i + 1);
                 }
                 returnArrayList.add(tempArray);
             }
             // cleaning up
             cstmt.close();
             rs.close();
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return returnArrayList;
@@ -73,38 +78,38 @@ public class DB {
 
     //TODO method to execute batch stored procedure + Commit method
 
-    private static ArrayList<ProcedureMetaData>  getSPMetaData(String sp){
+    private static ArrayList<ProcedureMetaData> getSPMetaData(String sp) {
         ArrayList<ProcedureMetaData> returnArrayList = new ArrayList<>();
         try {
             //Establishing call
             cstmt = conn.prepareCall("{call sp_GetSPMetaData(?)}");
-            cstmt.setString(1,sp.toString());
+            cstmt.setString(1, sp);
             rs = cstmt.executeQuery();
             // Getting the Metadata
-            while (rs.next()){
-                int position = (int)rs.getObject("ORDINAL_POSITION");
-                java.lang.String type = (java.lang.String)rs.getObject("DATA_TYPE");
-                ProcedureMetaData tempMetaData = new ProcedureMetaData(position,type);
+            while (rs.next()) {
+                int position = (int) rs.getObject("ORDINAL_POSITION");
+                java.lang.String type = (java.lang.String) rs.getObject("DATA_TYPE");
+                ProcedureMetaData tempMetaData = new ProcedureMetaData(position, type);
                 returnArrayList.add(tempMetaData);
             }
             // Cleaning up
             cstmt.close();
             rs.close();
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return returnArrayList;
     }
 
-    private static java.lang.String buildProcedureCall(String sp, int parameters){ //TODO yeah there probably a bug here
+    private static java.lang.String buildProcedureCall(String sp, int parameters) { //TODO yeah there probably a bug here
         StringBuilder builder = new StringBuilder();
         builder.append("{call ");
         builder.append(sp);
         if (parameters > 0) {
             builder.append(" (");
-            for (int questionMarks = parameters; questionMarks != 0; questionMarks--){
+            for (int questionMarks = parameters; questionMarks != 0; questionMarks--) {
                 builder.append("?");
-                if (questionMarks > 1){
+                if (questionMarks > 1) {
                     builder.append(",");
                 }
             }
@@ -114,45 +119,53 @@ public class DB {
         return builder.toString();
     }
 
-    private static void setCallParameter(int index, java.lang.String dataType, Object dataValue){
+    private static void setCallParameter(int index, String dataType, Object dataValue) {
         try {
-            if (dataType.equals("int")) {
-                cstmt.setInt(index, (int) dataValue);
-            } else if (dataType.equals("varchar") || dataType.equals("nvarchar")){
-                cstmt.setString(index,(java.lang.String) dataValue);
-            } else if (dataType.equals("numeric")){
-                cstmt.setDouble(index,(double) dataValue);
-            } else if (dataType.equals("date")){
-                cstmt.setDate(index,(Date)dataValue);
+            switch (dataType) {
+                case "int":
+                    cstmt.setInt(index, (int) dataValue);
+                    break;
+                case "varchar":
+                case "nvarchar":
+                    cstmt.setString(index, (String) dataValue);
+                    break;
+                case "numeric":
+                    cstmt.setBigDecimal(index, (BigDecimal) dataValue);
+                    break;
+                case "date":
+                    cstmt.setDate(index, (Date) dataValue);
+                    break;
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static void connect(){
+    public static void connect() {
         try {
-            conn = DriverManager.getConnection("jdbc:sqlserver://localhost:"+port+";databaseName="+databaseName, userName, password);
+            conn = DriverManager.getConnection("jdbc:sqlserver://localhost:" + port + ";databaseName=" + databaseName, userName, password);
         } catch (SQLException e) { //TODO error handling could be improved in this class in genneral
             e.printStackTrace();
         }
     }
 
-    public static void disconnect(){
-        try{
-            if (cstmt != null){
+    public static void disconnect() {
+        try {
+            if (cstmt != null) {
                 cstmt.close();
             }
-            if(conn != null){
+            if (conn != null) {
                 conn.close();
             }
-            if (rs != null){
+            if (rs != null) {
                 rs.close();
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-
+    public static ResultSetMetaData getRsmd() {
+        return rsmd;
+    }
 }
